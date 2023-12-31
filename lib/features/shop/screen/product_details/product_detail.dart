6e457ad5ac092589958551ff30dc6b9603914715
Zetah07 <sebastiancastro0792@gmail.com>
2023-12-31 00:17:28 -0500

    Add product attributes widget to product detail screen

--- lib/features/shop/screen/product_details/product_detail.dart
+++ lib/features/shop/screen/product_details/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:js_goods/features/shop/screen/product_details/widgets/product_attributes.dart';
 
 import '../../../../utils/constants/sizes.dart';
 import 'widgets/product_image_slider.dart';
@@ -31,7 +32,9 @@ class ProductDetailScreen extends StatelessWidget {
 
                 //       //Price, Title, Stock & Brand
                 TProductMetaData(),
+
                 //       //Attributes
+                TProductAttributes(),
                 //       //Checkout Button
                 //       //Description
                 //       //Reviews
---
+++ lib/features/shop/screen/product_details/widgets/product_attributes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:js_goods/common/widgets/images/t_rounded_container.dart';
+
+class TProductAttributes extends StatelessWidget {
+  const TProductAttributes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        //Select Attribute Pricing & Description
+        TRoundedContainer(
+          
+        )
+      ],
+    );
+  }
+}
