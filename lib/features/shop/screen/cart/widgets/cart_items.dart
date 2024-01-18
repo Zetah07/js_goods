e9b05dedfc5137d7d1b86666299779253186f459
Zetah07 <sebastiancastro0792@gmail.com>
2024-01-18 16:47:57 -0500

    Update import statement for cart item widget

--- lib/features/shop/screen/cart/widgets/cart_items.dart
+++ lib/features/shop/screen/cart/widgets/cart_items.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../common/widgets/products/cart/add_remove_button.dart';
-import '../../../../../common/widgets/products/cart/t_cart_item.dart';
+import '../../../../../common/widgets/products/cart/cart_item.dart';
 import '../../../../../common/widgets/texts/product_price_text.dart';
 import '../../../../../utils/constants/sizes.dart';
 
